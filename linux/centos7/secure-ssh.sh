a95be2c934e09de6254795a09be289ce51f1ce26
kerryallen89 <kerry.allen@mymail.champlain.edu>
2026-03-20 02:35:58 +0000

    Script for passwordless user

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,6 +1,25 @@
+#!/bin/bash
+
 #secure-ssh.sh
 #author kerryallen89
 #creates a new ssh user using $1 parameters
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
-echo "ALL YOUR CODE GOES HERE"
+
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/kerry/SYS-265-01
+sudo cp linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
+
+# Blocks Root SSH Login
+if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
+   sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+else
+   echo "PermitRootLogin not found in /etc/ssh/sshd_config"
+fi
+
+# Restarts the SSH Service
+sudo service ssh restart
